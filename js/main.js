6adc54af211d622cf63386327d203b9c78c9b5e2
wiolettamehiadin <wioletta_k@live.co.uk>
2021-05-23 22:34:46 +0100

    Rozpoczecie pliku

---
+++ js/main.js
@@ -0,0 +1,10 @@
+    let n = 120;
+    if (n<100) {
+        console.log('Liczba nie znajduje się w przedziale');
+    }
+    else if (n>100<200) {
+        console.log('Liczna znajduje sie w przedziale');
+    }
+        
+
+console.log(n);
